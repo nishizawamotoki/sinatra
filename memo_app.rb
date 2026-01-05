de7b0d94080e461fd7fdd19153aa4c5a7ed60810
nishizawamotoki <sokomichi.hizawa@gmail.com>
2026-01-06 00:37:38 +0900

    データアクセス部分をメソッド化した

--- memo_app.rb
+++ memo_app.rb
@@ -18,15 +18,13 @@ helpers do
 end
 
 get '/memos' do
-  @memos = JSON.load_file(DATA_FILE)
-
+  @memos = find_memos
   erb :index
 end
 
 get '/memos/new' do
-  memos = JSON.load_file(DATA_FILE)
+  memos = find_memos
   @new_id = memos.empty? ? 1 : memos.last['id'].to_i + 1
-
   erb :new
 end
 
@@ -53,40 +51,21 @@ get '/memos/:id/edit' do
 end
 
 post '/memos' do
-  old_memos = JSON.load_file(DATA_FILE)
-  new_memos = [*old_memos, { 'id': params['id'], 'title': params['title'], 'content': params['content'] }]
-  File.write(DATA_FILE, JSON.generate(new_memos))
-
+  create_memo(params['id'], params['title'], params['content'])
   redirect to('/memos'), 303
 end
 
 patch '/memos/:id' do
   halt 400 if find_memo(params['id']).nil?
 
-  old_memos = JSON.load_file(DATA_FILE)
-  new_memos = old_memos.map do |memo|
-    if memo['id'] == params['id']
-      {
-        **memo,
-        'title' => params['title'],
-        'content' => params['content']
-      }
-    else
-      memo
-    end
-  end
-  File.write(DATA_FILE, JSON.generate(new_memos))
-
+  update_memo(params['id'], params['title'], params['content'])
   redirect to("/memos/#{params['id']}"), 303
 end
 
 delete '/memos/:id' do
   halt 400 if find_memo(params['id']).nil?
 
-  old_memos = JSON.load_file(DATA_FILE)
-  new_memos = old_memos.filter { |memo| memo['id'] != params['id'] }
-  File.write(DATA_FILE, JSON.generate(new_memos))
-
+  delete_memo(params['id'])
   redirect to('/memos'), 303
 end
 
@@ -95,6 +74,38 @@ not_found do
 end
 
 def find_memo(id)
-  memos = JSON.load_file(DATA_FILE)
+  memos = find_memos
   memos.find { |memo| memo['id'] == id }
+end
+
+def find_memos
+  JSON.load_file(DATA_FILE)
+end
+
+def create_memo(id, title, content)
+  old_memos = find_memos
+  new_memos = [*old_memos, { 'id': id, 'title': title, 'content': content }]
+  File.write(DATA_FILE, JSON.generate(new_memos))
+end
+
+def update_memo(id, title, content)
+  old_memos = find_memos
+  new_memos = old_memos.map do |memo|
+    if memo['id'] == id
+      {
+        **memo,
+        'title' => title,
+        'content' => content
+      }
+    else
+      memo
+    end
+  end
+  File.write(DATA_FILE, JSON.generate(new_memos))
+end
+
+def delete_memo(id)
+  old_memos = find_memos
+  new_memos = old_memos.filter { |memo| memo['id'] != id }
+  File.write(DATA_FILE, JSON.generate(new_memos))
 end
